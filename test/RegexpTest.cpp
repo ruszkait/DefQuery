0b4659a265f37f93ccb8efd51c725b4a7b7934e8
tamas <ruszkait@gmail.com>
2020-11-21 14:29:08 +0100

    unit test improvement

--- test/RegexpTest.cpp
+++ test/RegexpTest.cpp
@@ -1,13 +1,15 @@
-#include "gtest/gtest.h"
 #include <DefQuery/orderby.h>
 #include <DefQuery/range.h>
 #include <DefQuery/select.h>
 #include <DefQuery/where.h>
 #include <ctime>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include <list>
 #include <regex>
 #include <string>
 
+using namespace ::testing;
 TEST(RegexpTest, IntegerRegisterRegexpTest)
 {
 	std::string text = "realValue=#r55# integerValue=#i46# badFormedValue=#55# integerValue=#i59# "
@@ -37,41 +39,41 @@ TEST(RegexpTest, IntegerRegisterRegexpTest)
 	ASSERT_FALSE(++integerReferenceNumbersEnumerator);
 }
 
-TEST(RegexpTest, DateTimeRegexpTest)
+TEST(RegexpTest, ComplexDataRegexpTest)
 {
-	std::string text = "15:19:00 17/06/2018, 14:19:00 16/06/2016, 14:19:00";
-	std::regex pattern("(([0-9]+):([0-9]+):([0-9]+)) (([0-9]+)/([0-9]+)/([0-9]+))");
+	std::string text = "Peter 2000, Mary 1995, John 2010";
+	std::regex pattern("([[:alpha:]]+) ([[:digit:]]+)");
+
+	struct Person
+	{
+		std::string name;
+		std::size_t birth_year;
+	};
 
 	auto dateTimeEnumerator =
 		DefQuery::range(std::sregex_iterator(text.begin(), text.end(), pattern),
 						std::sregex_iterator())
 			.select([](auto& matchIter) {
-				std::tm timeStruct;
-
-				// index 0: the full match, like: 15:19:00 17/06/2018
-				// further indices are the count of the starting parenthesises
-
-				timeStruct.tm_hour = std::stoi((*matchIter)[2]);
-				timeStruct.tm_min = std::stoi((*matchIter)[3]);
-				timeStruct.tm_sec = std::stoi((*matchIter)[4]);
-
-				timeStruct.tm_mday = std::stoi((*matchIter)[6]);
-				timeStruct.tm_mon = std::stoi((*matchIter)[7]) - 1;
-				timeStruct.tm_year = std::stoi((*matchIter)[8]) - 1900;
-
-				auto timeSinceEpoch = std::mktime(&timeStruct);
-				return timeSinceEpoch;
+				Person person;
+				person.name = (*matchIter)[1];
+				person.birth_year = std::stoi((*matchIter)[2]);
+				return person;
 			})
-			.orderby(DefQuery::sorting_order::ascending)
-			.select([](auto& timeSinceEpoch) {
-				auto timeStruct = *std::localtime(&timeSinceEpoch);
-				return std::string(std::asctime(&timeStruct));
+			.orderby(DefQuery::sorting_order::ascending,
+					 [](const Person& a, const Person& b) { return a.birth_year > b.birth_year; })
+			.select([](const Person& person) {
+				std::stringstream personTextStream;
+				personTextStream << person.name << " was born in " << person.birth_year;
+
+				return personTextStream.str();
 			});
 
 	ASSERT_TRUE(++dateTimeEnumerator);
-	ASSERT_EQ("Thu Jun 16 14:19:00 2016\n", *dateTimeEnumerator);
+	ASSERT_THAT(*dateTimeEnumerator, StrEq("Mary was born in 1995"));
 	ASSERT_TRUE(++dateTimeEnumerator);
-	ASSERT_EQ("Sun Jun 17 15:19:00 2018\n", *dateTimeEnumerator);
+	ASSERT_THAT(*dateTimeEnumerator, StrEq("Peter was born in 2000"));
+	ASSERT_TRUE(++dateTimeEnumerator);
+	ASSERT_THAT(*dateTimeEnumerator, StrEq("John was born in 2010"));
 	ASSERT_FALSE(++dateTimeEnumerator);
 	ASSERT_FALSE(++dateTimeEnumerator);
 }
